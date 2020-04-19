0d17a91a409b5b125ce91c6766361ad330d702c2
yuta sakamoto <sorigeeeen@gmail.com>
2020-04-19 16:55:57 +0900

    make top page frame

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+# ポートフォリオトップページ
+class HomeView(TemplateView):
+  template_name = 'portfolio/home.html'
+
+home = HomeView.as_view()
